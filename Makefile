ddf9acab6ad1d0092ea8ebbce92b8e1f2b9ca40e
Aleksey Kononenko <aleksey19988@gmail.com>
2020-12-19 13:07:32 +0400

    add brain-calc

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ lint:
 	composer run-script phpcs -- --standard=PSR12 src bin
 brain-even:
 	./bin/brain-even
+brain-calc:
+	./bin/brain-calc
--- utilites/random-num.php
+++ utilites/random-num.php
@@ -2,11 +2,6 @@
 
 namespace Utilites\RandomNum;
 
-function randomNum()
-{
-    return random_int(0, 100);
-}
-
 function isEven($num) {
     return $num % 2 === 0;
 }
