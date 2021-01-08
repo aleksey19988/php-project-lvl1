2225219f73b8403538512cb5b250ff67d00ebe9f
Aleksey Kononenko <aleksey19988@gmail.com>
2021-01-08 12:50:14 +0300

    добавил engine.php

---
+++ src/Engine.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Src\Brain\Games\Engine;
+
+use function Brain\Games\Cli\greeting;
+use function cli\line;
+use function cli\prompt;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+function runGame($rules, $round, int $attempts = 3)
+{
+    $name = greeting();
+    line($rules);
+
+    for ($i = 0; $i < $attempts; $i += 1) {
+        ['question' => $question, 'correctAnswer' => $correctAnswer] = $round();
+
+        line("{$question}\n");
+        $answer = prompt('Your answer');
+
+        if ($answer === (string) $correctAnswer) {
+            line("Correct!\n");
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'\nLet's try again, {$name}");
+            return;
+        }
+    }
+    line("Congratulations, {$name}!\n");
+}
